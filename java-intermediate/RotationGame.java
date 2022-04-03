7c1d36915cdf6d2697993d6cb2186ccbf01edaea
Jayadeep217 <gaddamjayadeep@gmail.com>
2022-04-03 12:56:17 +0530

    Added reverseArray & RotationGame

--- java-intermediate/RotationGame.java
+++ java-intermediate/RotationGame.java
@@ -9,16 +9,17 @@ class RotationGame {
         }
         System.out.println();
     }
-    public static void reverse(int[] A) {
-        for(int i = 0; i<A.length; i++){
-            for(int j = i+1; j<A.length; j++){
-                int temp = A[i];
-                A[i] = A[j];
-                A[j] = temp;
-            }
+    public static void reverse(int[] A, int startIndex, int endIndex) {
+        int i = startIndex;
+        int j = endIndex;
+        while(i<j){
+            int temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+            i++;
+            j--;
         }
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int arrayLength = sc.nextInt();
@@ -26,8 +27,16 @@ class RotationGame {
         for(int a = 0; a<arrayLength; a++){
             arr[a] = sc.nextInt();
         }
+        int rotations = sc.nextInt();
+        if(rotations > arrayLength){
+            rotations = rotations % arrayLength;
+        }
         //reverse
-        reverse(arr);
+        reverse(arr,0,arrayLength - 1);
+        //reverse 0 to k = rotations
+        reverse(arr,0,rotations - 1);
+        //reverse k to N-K
+        reverse(arr,rotations,arrayLength - 1);
         //print
         printArray(arr);
         sc.close();        

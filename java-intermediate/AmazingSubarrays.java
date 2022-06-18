05b021b3a5153fefa5d29d31cff557831a9a39d9
Jayadeep217 <gaddamjayadeep@gmail.com>
2022-06-18 16:26:33 +0530

    fixed

--- java-intermediate/AmazingSubarrays.java
+++ java-intermediate/AmazingSubarrays.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 
 public class AmazingSubarrays {
     public static int solve(String A) {
-        int res = 0;
-        int size = A.length();
-        String vowels = "aeiouAEIOU";
-        for(int i = 0;i<size;i++){
-            if(vowels.contains(String.valueOf(A.charAt(i))))
-            res += size - i;
+        int n = A.length();
+        int sum = 0;
+        for(int i =0;i<n;i++){
+            char ch = A.charAt(i);
+            if(ch == 'a' ||ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ){
+                sum +=n-i;
+                sum%=10003;
+            }
         }
-        return res%10003;
+        return sum%10003;
     }
 
     public static void main(String[] args) {
